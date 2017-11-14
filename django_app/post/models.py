7f34b11c6d97520ae22f0997e9f3bd880258417b
thenry <thenry0401@gmail.com>
2017-11-14 17:04:54 +0900

    add Post,Comment,Tag models

--- django_app/post/models.py
+++ django_app/post/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(User)
+    photo = models.ImageField(blank=True)
+    my_comment = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    like_users = models.ManyToManyField(
+        User,
+        related_name='like_posts'
+    )
+    tags = models.ManyToManyField('Tag')
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post)
+    author = models.ForeignKey(User)
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return 'Tag({})'.format(self.name)
