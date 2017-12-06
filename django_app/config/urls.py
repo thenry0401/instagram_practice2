4b53944d32788c0cd7726e965d3f2b5fa4309c7c
thenry <thenry0401@gmail.com>
2017-12-06 23:44:14 +0900

    post앱에서index뷰 만들고url 연결

--- django_app/config/urls.py
+++ django_app/config/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from post import views as post_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
+    url(r'^$', post_views.index),
 ]
