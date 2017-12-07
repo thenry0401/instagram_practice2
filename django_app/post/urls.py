26bcaa700f3dd5aac1e86992e2b8eebe38f0d1cf
thenry <thenry0401@gmail.com>
2017-12-07 23:59:56 +0900

    post list add

--- django_app/post/urls.py
+++ django_app/post/urls.py
@@ -4,4 +4,5 @@ from post import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'')
 ]
--- django_app/post/views.py
+++ django_app/post/views.py
@@ -1,6 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from post.models import Post
+
 
 def index(request):
     return HttpResponse('Hello, world')
+
+
+def post_list(request):
+    post = Post.objects.all()
+    context = {
+        'post': post,
+    }
+    return render(request, 'post/post_list.html', context)
