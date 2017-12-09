f495ef0f14dcce609878e6fd27c39ab30e942cf5
thenry <thenry0401@gmail.com>
2017-12-09 23:59:13 +0900

    post 관련 뷰 추가

--- django_app/post/views.py
+++ django_app/post/views.py
@@ -14,3 +14,20 @@ def post_list(request):
         'posts': posts,
     }
     return render(request, 'post/post_list.html', context)
+
+
+def post_detail(request):
+    pass
+
+
+def post_create(request):
+    pass
+
+
+def post_modify(request):
+    pass
+
+
+def post_delete(request):
+    pass
+
