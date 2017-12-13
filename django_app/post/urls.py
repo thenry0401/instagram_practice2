acc7925b1432c260db4afada24e73a27f9c84a0b
thenry <thenry0401@gmail.com>
2017-12-13 23:14:38 +0900

    url에 이름 추가

--- django_app/post/urls.py
+++ django_app/post/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from post import views
 
 urlpatterns = [
-    url(r'^$', views.post_list),
+    url(r'^$', views.post_list, name='post_list'),
 
-    url(r'^(?P<post_pk>\d+)/$', views.post_detail)
+    url(r'^(?P<post_pk>\d+)/$', views.post_detail, name='post_detail')
 ]
